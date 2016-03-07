c32c2670df2197a7ed48149c8073018dfc8238b8
matianfu <matianfu@gmail.com>
2016-03-07 12:27:21 +0800

    tut003, serve html pages

---
+++ examples/tut003/hello.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+
+	if (!responseCode) responseCode = 200;
+
+	console.log(__dirname + path);
+	fs.readFile(__dirname + path, (err, data) => {
+		if (err) {
+			res.writeHead(500, {'Content-Type' : 'text/plain'});
+			res.end('500 - Internal Error');
+		}
+		else {
+			res.writeHead(responseCode, { 'Content-Type' : contentType });
+			res.end(data);
+		}
+	})
+}
+
+http.createServer( (req, res) => {
+
+	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+
+	console.log("req path: " + path);
+	switch(path) {
+		case '':
+			serveStaticFile(res, '/public/home.html', 'text/html');
+			break;
+		case '/about':
+			serveStaticFile(res, '/public/about.html', 'text/html');
+			break;
+		case '/img/logo.jpg':
+			serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
+			break;
+		default:
+			serveStaticFile(res, '/public/404.html', 'text/html', 404);
+			break;
+	}
+}).listen(3000);
+
+console.log("Server started on port 3000....");
+
